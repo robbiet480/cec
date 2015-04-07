b4a63d56e896aa3e6264c77aa6061eda17f65213
Robbie Trencheny <me@robbiet.us>
2015-04-07 11:55:53 -0700

    Improve log messages

--- callbacks.go
+++ callbacks.go
@@ -10,7 +10,27 @@ import (
 
 //export logMessageCallback
 func logMessageCallback(c unsafe.Pointer, msg C.cec_log_message) C.int {
-	log.Println(C.GoString(&msg.message[0]))
+  var logPrefix string
+  switch (msg.level) {
+    case C.CEC_LOG_ERROR:
+      logPrefix = "ERROR:   ";
+      break;
+    case C.CEC_LOG_WARNING:
+      logPrefix = "WARNING: ";
+      break;
+    case C.CEC_LOG_NOTICE:
+      logPrefix = "NOTICE:  ";
+      break;
+    case C.CEC_LOG_TRAFFIC:
+      logPrefix = "TRAFFIC: ";
+      break;
+    case C.CEC_LOG_DEBUG:
+      logPrefix = "DEBUG:   ";
+      break;
+    default:
+      break;
+  }
+	log.Println(logPrefix+C.GoString(&msg.message[0]))
 
 	return 0
 }
